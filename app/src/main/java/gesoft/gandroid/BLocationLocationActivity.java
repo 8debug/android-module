bafd894d849768554f4639d1455f42ea3d0f290a
297179121 <297179121>
2016-06-30 16:01:31 +0800

    百度定位

---
+++ app/src/main/java/gesoft/gandroid/BLocationLocationActivity.java
@@ -0,0 +1,41 @@
+package gesoft.gandroid;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import gesoft.gapp.common.T;
+import gesoft.gbmap.GBLocation;
+import gesoft.gbmap.activity.GBLocationActivity;
+
+public class BLocationLocationActivity extends GBLocationActivity {
+
+    @Override
+    protected void onCreateG(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_blocation);
+    }
+
+    public void location(final View view){
+        final Button btn = (Button)view;
+        mGBLocation.setIGBLocation(new GBLocation.IGBLocation() {
+            @Override
+            public void onStart() {
+                btn.setText(getResources().getString(R.string.btn_bd_start));
+            }
+
+            @Override
+            public void onFinish(boolean isSuccess, GBLocation.GLBean bean) {
+                mGBLocation.stop();
+                btn.setText(getResources().getString(R.string.btn_bd_stop));
+                if( isSuccess ){
+                    TextView tv = (TextView)findViewById(R.id.tv_result);
+                    tv.setText(bean.getDiscribe());
+                }
+                T.show(mContext, bean.getMsg()+" "+bean.getTime());
+            }
+        });
+
+        mGBLocation.start();
+    }
+}
--- app/src/main/java/gesoft/gandroid/MainActivity.java
+++ app/src/main/java/gesoft/gandroid/MainActivity.java
@@ -6,7 +6,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import gesoft.gandroid.download.service.DownloadService;
+import gesoft.gphotoview.GPhotoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mPro = new ProgressDialog(this);
+
+
     }
 
     public void download(View view){
@@ -25,6 +31,22 @@ public class MainActivity extends AppCompatActivity {
 
     public void retrofit(View view){
         startActivity( new Intent(this, RetrofitActivity.class));
+    }
+
+    public void photoview(View view){
+
+        List<String> listUrl = new ArrayList<>();
+
+        listUrl.add("http://ww1.sinaimg.cn/mw600/b4ece975tw1e33ep5rqfmj.jpg");
+        listUrl.add("http://img.eeyy.com/uploadfile/2015/1215/20151215091108369.jpg");
+        listUrl.add("http://tupian.enterdesk.com/2013/lxy/07/27/6/1.jpg");
+        listUrl.add("http://dl.bizhi.sogou.com/images/2012/04/16/113574.jpg");
+
+        GPhotoView.startImagePagerActivity(this, listUrl, 0, view.getMeasuredWidth(), view.getMeasuredHeight());
+    }
+
+    public void bdlocation(View view){
+        startActivity(new Intent(this, BLocationLocationActivity.class));
     }
 
 }
