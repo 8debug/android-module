1be407b783f0a6f17a274ffa86e9888708d9ed2a
297179121 <297179121>
2016-08-12 11:00:11 +0800

    新增GPhone类用来获取手机信息

--- app/src/main/java/gesoft/gandroid/MainActivity.java
+++ app/src/main/java/gesoft/gandroid/MainActivity.java
@@ -36,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        T.setContext(this);
         mPro = new ProgressDialog(this);
         mContext = this;
         gpush();
