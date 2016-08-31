dab84542184c9b49963eb8db2165cacee2ce6e2e
297179121 <297179121>
2016-08-31 09:19:55 +0800

    图片预览

--- gphotoview/src/main/java/gesoft/gphotoview/GPhotoView.java
+++ gphotoview/src/main/java/gesoft/gphotoview/GPhotoView.java
@@ -1,16 +1,23 @@
 package gesoft.gphotoview;
 
 import android.content.Context;
+import android.view.View;
 
 import java.util.List;
 
 /**
- * Created by Administrator on 2016/6/27.
+ * Created by yhr on 2016/6/27.
+ *
  */
 public class GPhotoView {
 
     public static void startImagePagerActivity(Context context, List<String> imgUrls, int position, int width, int height){
         ImagePagerActivity.startImagePagerActivity(context, imgUrls, position, width, height);
+
+    }
+
+    public static void startImagePagerActivity( View view, List<String> imgUrls, int position ){
+        ImagePagerActivity.startImagePagerActivity( view.getContext(), imgUrls, position, view.getMeasuredWidth(), view.getMeasuredHeight());
 
     }
 
