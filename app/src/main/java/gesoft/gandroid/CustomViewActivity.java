a8c3da9ccb63d4d64d82dd09ea9a6c3266af694e
297179121 <297179121@qq.com>
2016-11-14 17:30:03 +0800

    绘制图形demo

--- app/src/main/java/gesoft/gandroid/CustomViewActivity.java
+++ app/src/main/java/gesoft/gandroid/CustomViewActivity.java
@@ -1,13 +1,53 @@
 package gesoft.gandroid;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class CustomViewActivity extends Activity {
+
+    @Bind(R.id.img)
+    ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_view);
+        ButterKnife.bind(this);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.a);
+        bitmap = createCircleImage(bitmap, 200  );
+        img.setImageBitmap(bitmap);
+    }
+
+    /**
+     * 根据原图和变长绘制圆形图片
+     *
+     * @param source
+     * @param min
+     * @return
+     */
+    private Bitmap createCircleImage(Bitmap source, int min) {
+        final Paint paint = new Paint();
+        paint.setAntiAlias(true);
+        // 注意一定要用ARGB_8888，否则因为背景不透明导致遮罩失败
+        Bitmap target = Bitmap.createBitmap(min, min, Bitmap.Config.ARGB_8888);
+        // 产生一个同样大小的画布
+        Canvas canvas = new Canvas(target);
+        // 首先绘制圆形
+        canvas.drawCircle(min / 2, min / 2, min / 2, paint);
+        // 使用SRC_IN
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        // 绘制图片
+        canvas.drawBitmap(source, 0, 0, paint);
+        return target;
     }
 }
