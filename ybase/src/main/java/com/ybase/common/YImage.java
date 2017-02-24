0264cc6d327f4c26138a0bca2c91f909c4ae5bc2
297179121 <297179121@qq.com>
2017-02-24 14:44:55 +0800

    基础类

---
+++ ybase/src/main/java/com/ybase/common/YImage.java
@@ -0,0 +1,401 @@
+package com.ybase.common;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.Typeface;
+import android.widget.ImageView;
+
+import com.ybase.common.log.L;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by yhr on 2016/2/15.
+ *
+ */
+public class YImage {
+
+    /**
+     * 压缩图片
+     * @param path
+     * @return
+     */
+    public static Bitmap revitionImageSize(String path){
+        try {
+            BufferedInputStream in = new BufferedInputStream( new FileInputStream(path));
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = true;
+            BitmapFactory.decodeStream(in, null, options);
+            in.close();
+            int i=0;
+            Bitmap bitmap = null;
+            while (true){
+                if ( options.outWidth>>i <=1000 && options.outHeight>>i <=1000 ){
+                    in = new BufferedInputStream(new FileInputStream(path));
+                    options.inSampleSize = (int)Math.pow(2.0D, i);
+                    options.inJustDecodeBounds = false;
+                    bitmap = BitmapFactory.decodeStream(in, null, options);
+                    /*L.d("options.outWidth==="+options.outWidth);
+                    L.d("options.outHeight==="+options.outHeight);*/
+                    break;
+                }
+                i++;
+            }
+            return bitmap;
+        } catch (Exception e) {
+            L.e(e);
+        }
+        return null;
+    }
+
+    /**
+     * Get bitmap from specified image path
+     *
+     * @param imgPath
+     * @return
+     */
+    public static Bitmap getBitmap(String imgPath) {
+        // Get bitmap through image path
+        BitmapFactory.Options newOpts = new BitmapFactory.Options();
+        newOpts.inJustDecodeBounds = false;
+        newOpts.inPurgeable = true;
+        newOpts.inInputShareable = true;
+        // Do not compress
+        newOpts.inSampleSize = 1;
+        newOpts.inPreferredConfig = Bitmap.Config.RGB_565;
+        return BitmapFactory.decodeFile(imgPath, newOpts);
+    }
+
+    /**
+     * Store bitmap into specified image path
+     *
+     * @param bitmap
+     * @param outPath
+     * @throws FileNotFoundException
+     */
+    public static void storeImage(Bitmap bitmap, String outPath){
+        try{
+            FileOutputStream os = new FileOutputStream(outPath);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, os);
+            os.flush();
+            os.close();
+        }catch (Exception e){
+            L.e(e);
+        }
+    }
+
+    /**
+     * Compress image by pixel, this will modify image width/height.
+     * Used to get thumbnail
+     *
+     * @param imgPath image path
+     * @param pixelW target pixel of width
+     * @param pixelH target pixel of height
+     * @return
+     */
+    public static Bitmap ratio(String imgPath, float pixelW, float pixelH) {
+        BitmapFactory.Options newOpts = new BitmapFactory.Options();
+        // 开始读入图片，此时把options.inJustDecodeBounds 设回true，即只读边不读内容
+        newOpts.inJustDecodeBounds = true;
+        newOpts.inPreferredConfig = Bitmap.Config.RGB_565;
+        // Get bitmap info, but notice that bitmap is null now
+        Bitmap bitmap = BitmapFactory.decodeFile(imgPath,newOpts);
+
+        newOpts.inJustDecodeBounds = false;
+        int w = newOpts.outWidth;
+        int h = newOpts.outHeight;
+        // 想要缩放的目标尺寸
+        float hh = pixelH;// 设置高度为240f时，可以明显看到图片缩小了
+        float ww = pixelW;// 设置宽度为120f，可以明显看到图片缩小了
+        // 缩放比。由于是固定比例缩放，只用高或者宽其中一个数据进行计算即可
+        int be = 1;//be=1表示不缩放
+        if (w > h && w > ww) {//如果宽度大的话根据宽度固定大小缩放
+            be = (int) (newOpts.outWidth / ww);
+        } else if (w < h && h > hh) {//如果高度高的话根据宽度固定大小缩放
+            be = (int) (newOpts.outHeight / hh);
+        }
+        if (be <= 0) be = 1;
+        newOpts.inSampleSize = be;//设置缩放比例
+        // 开始压缩图片，注意此时已经把options.inJustDecodeBounds 设回false了
+        bitmap = BitmapFactory.decodeFile(imgPath, newOpts);
+        // 压缩好比例大小后再进行质量压缩
+//        return compress(bitmap, maxSize); // 这里再进行质量压缩的意义不大，反而耗资源，删除
+        return bitmap;
+    }
+
+
+    public static Bitmap ratioDefault(Bitmap bitmap, Integer size){
+        size = size==null?1280:size;
+        BitmapFactory.Options opts = new BitmapFactory.Options();
+        opts.inJustDecodeBounds = true;
+        opts.inPreferredConfig = Bitmap.Config.RGB_565;
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, os);
+        byte[] bytes = os.toByteArray();
+        BitmapFactory.decodeByteArray(bytes, 0, bytes.length, opts);
+        opts.inJustDecodeBounds = false;
+
+        int actWidth = opts.outWidth;
+        int actHeight = opts.outHeight;
+
+
+        int max = Math.max(actWidth, actHeight);
+        int min = Math.min(actWidth, actHeight);
+        int ratio = 1;
+        int be = max/min;
+
+        if( max<=size ){
+            return bitmap;
+        }
+        //实际宽和高>1280
+        else if( min>size && be>2 ){
+            ratio = min/size;
+        }
+        //实际宽或高>1280
+        else if( (max>size || min>size) && be<=2 ){
+            ratio = max/size;
+        }
+
+        opts.inSampleSize = ratio;
+
+        InputStream inpt = new ByteArrayInputStream(bytes);
+        return BitmapFactory.decodeStream(inpt, null, opts);
+    }
+
+    /**
+     * Compress image by size, this will modify image width/height.
+     * Used to get thumbnail
+     *
+     * @param image
+     * @param pixelW target pixel of width
+     * @param pixelH target pixel of height
+     * @return
+     */
+    public static Bitmap ratio(Bitmap image, float pixelW, float pixelH) {
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        image.compress(Bitmap.CompressFormat.JPEG, 100, os);
+        if( os.toByteArray().length / 1024>1024) {//判断如果图片大于1M,进行压缩避免在生成图片（BitmapFactory.decodeStream）时溢出
+            os.reset();//重置baos即清空baos
+            image.compress(Bitmap.CompressFormat.JPEG, 50, os);//这里压缩50%，把压缩后的数据存放到baos中
+        }
+        BitmapFactory.Options newOpts = new BitmapFactory.Options();
+        //开始读入图片，此时把options.inJustDecodeBounds 设回true了
+        newOpts.inJustDecodeBounds = true;
+        newOpts.inPreferredConfig = Bitmap.Config.RGB_565;
+        newOpts.inJustDecodeBounds = false;
+        int w = newOpts.outWidth;
+        int h = newOpts.outHeight;
+        float hh = pixelH;// 设置高度为240f时，可以明显看到图片缩小了
+        float ww = pixelW;// 设置宽度为120f，可以明显看到图片缩小了
+        //缩放比。由于是固定比例缩放，只用高或者宽其中一个数据进行计算即可
+        int be = 1;//be=1表示不缩放
+        if (w > h && w > ww) {//如果宽度大的话根据宽度固定大小缩放
+            be = (int) (newOpts.outWidth / ww);
+        } else if (w < h && h > hh) {//如果高度高的话根据宽度固定大小缩放
+            be = (int) (newOpts.outHeight / hh);
+        }
+        if (be <= 0) be = 1;
+        newOpts.inSampleSize = be;//设置缩放比例
+        //重新读入图片，注意此时已经把options.inJustDecodeBounds 设回false了
+        ByteArrayInputStream is = new ByteArrayInputStream(os.toByteArray());
+        Bitmap bitmap = BitmapFactory.decodeStream(is, null, newOpts);
+        //压缩好比例大小后再进行质量压缩
+//      return compress(bitmap, maxSize); // 这里再进行质量压缩的意义不大，反而耗资源，删除
+        return bitmap;
+    }
+
+    /**
+     * Compress by quality,  and generate image to the path specified
+     *
+     * @param image
+     * @param outPath
+     * @param maxSize target will be compressed to be smaller than this size.(kb)
+     * @throws IOException
+     */
+    public static void compressAndGenImage(Bitmap image, String outPath, int maxSize) throws IOException {
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        // scale
+        int options = 100;
+        // Store the bitmap into output stream(no compress)
+        image.compress(Bitmap.CompressFormat.JPEG, options, os);
+        // Compress by loop
+        while ( os.toByteArray().length / 1024 > maxSize) {
+            // Clean up os
+            os.reset();
+            // interval 10
+            options -= 10;
+            image.compress(Bitmap.CompressFormat.JPEG, options, os);
+        }
+
+        // Generate compressed image file
+        FileOutputStream fos = new FileOutputStream(outPath);
+        fos.write(os.toByteArray());
+        fos.flush();
+        fos.close();
+    }
+
+    /**
+     * Compress by quality,  and generate image to the path specified
+     *
+     * @param imgPath
+     * @param outPath
+     * @param maxSize target will be compressed to be smaller than this size.(kb)
+     * @param needsDelete Whether delete original file after compress
+     * @throws IOException
+     */
+    public static void compressAndGenImage(String imgPath, String outPath, int maxSize, boolean needsDelete) throws IOException {
+        compressAndGenImage(getBitmap(imgPath), outPath, maxSize);
+
+        // Delete original file
+        if (needsDelete) {
+            File file = new File (imgPath);
+            if (file.exists()) {
+                file.delete();
+            }
+        }
+    }
+
+    /**
+     * Ratio and generate thumb to the path specified
+     *
+     * @param image
+     * @param outPath
+     * @param pixelW target pixel of width
+     * @param pixelH target pixel of height
+     * @throws FileNotFoundException
+     */
+    public static void ratioAndGenThumb(Bitmap image, String outPath, float pixelW, float pixelH) throws Exception {
+        Bitmap bitmap = ratio(image, pixelW, pixelH);
+        storeImage( bitmap, outPath);
+    }
+
+    /**
+     * Ratio and generate thumb to the path specified
+     *
+     * @param imgPath
+     * @param outPath
+     * @param pixelW target pixel of width
+     * @param pixelH target pixel of height
+     * @param needsDelete Whether delete original file after compress
+     * @throws FileNotFoundException
+     */
+    public static void ratioAndGenThumb(String imgPath, String outPath, float pixelW, float pixelH, boolean needsDelete) throws Exception {
+        Bitmap bitmap = ratio(imgPath, pixelW, pixelH);
+        storeImage( bitmap, outPath);
+
+        // Delete original file
+        if (needsDelete) {
+            File file = new File(imgPath);
+            if (file.exists()) {
+                file.delete();
+            }
+        }
+    }
+
+
+    /**
+     * 使用fresco加载图片
+     * @param ctx
+     * @param path
+     * @param fresco
+     */
+    /*public static void loadImg(final Context ctx, final String path, final SimpleDraweeView fresco){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Bitmap bitmap = revitionImageSize(path);
+                    final File file = File.createTempFile(new Date().getTime()+"", ".jpg", new File(path).getParentFile());
+                    storeImage(bitmap, file.getPath());
+                    new Handler(ctx.getMainLooper()).post(new Runnable() {
+                        @Override
+                        public void run() {
+                            fresco.setImageURI(Uri.fromFile(file));
+                        }
+                    });
+                } catch (IOException e) {
+                    L.e(e);
+                }
+            }
+        }).start();
+    }*/
+
+    /**
+     * 加载本地图片（非高清大图）
+     * @param path
+     * @return
+     */
+    public static void setLocalImage( final String path, final ImageView imageView ){
+        try {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    final Bitmap bitmap = revitionImageSize(path);
+                    imageView.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            imageView.setImageBitmap(bitmap);
+                        }
+                    });
+                }
+            }).start();
+        } catch (Exception e) {
+            L.e(e);
+        }
+    }
+
+    public static Bitmap getBitmap( ImageView imageView ){
+        imageView.setDrawingCacheEnabled(true);
+        Bitmap bitmap = imageView.getDrawingCache();
+        imageView.setDrawingCacheEnabled(false);
+        return bitmap;
+    }
+
+    /**
+     * 添加文字水印
+     * @param bitmap
+     * @param text
+     */
+    public static  Bitmap addWaterMarker(Bitmap bitmap, String text, int textSize, float x, float y, int width, int height){
+
+        Bitmap icon = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(icon);
+
+        Paint photoPaint = new Paint(); //建立画笔
+        photoPaint.setDither(true); //获取跟清晰的图像采样
+        photoPaint.setFilterBitmap(true);//过滤一些
+
+        Rect src = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());//创建一个指定的新矩形的坐标
+        Rect dst = new Rect(0, 0, width, height);//创建一个指定的新矩形的坐标
+        canvas.drawBitmap(bitmap, src, dst, photoPaint);//将photo 缩放或则扩大到 dst使用的填充区photoPaint
+
+        Paint textPaint = new Paint(Paint.ANTI_ALIAS_FLAG | Paint.DEV_KERN_TEXT_FLAG);//设置画笔
+        textPaint.setTextSize(textSize);//字体大小
+        //textPaint.setTextAlign(Paint.Align.CENTER);
+        textPaint.setTypeface(Typeface.DEFAULT);//采用默认的宽度
+        textPaint.setAntiAlias(true);  //抗锯齿
+        textPaint.setStrokeWidth(0);
+        textPaint.setAlpha(0);
+        textPaint.setStyle(Paint.Style.FILL);
+        textPaint.setColor(Color.GRAY);//采用的颜色
+        textPaint.setShadowLayer(0f, 0f, 0f, Color.LTGRAY);
+
+        canvas.drawText(text, x, y, textPaint);//绘制上去字，开始未知x,y采用那只笔绘制
+        canvas.save(Canvas.ALL_SAVE_FLAG);
+        canvas.restore();
+        bitmap.recycle();
+        return icon;
+    }
+}
